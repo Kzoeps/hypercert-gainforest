fa64d7c01914ee3c8554d1959af2ea53f6d46f0a
kzoeps <kzoepa@gmail.com>
2025-07-20 23:24:33 +0600

    feat: add zod schema for payload

--- utils/attestation-constants.ts
+++ utils/attestation-constants.ts
@@ -1,3 +1,29 @@
-export const EAS_CONTRACT_ADDRESS = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e"; // Sepolia v0.26
+import { ethers } from "ethers";
+import * as z from "zod";
+
+export const EAS_CONTRACT_ADDRESS =
+  "0xC2679fBD37d54388Ce493F1DB75320D236e1815e"; // Sepolia v0.26
 export const SCHEMA_UID =
   "0x3a65facf06635b21432225452ac5a5c00bc300c91ff6f940dc7244836f892616";
+export const SCHEMA_PAYLOAD = z.object({
+  title: z
+    .string()
+    .min(5, { message: "Title must be greater than 5 characters" }),
+  description: z
+    .string()
+    .min(10, { message: "Description must be greater than 10 characters" }),
+  contributors: z
+    .array(z.string())
+    .nonempty({ message: "Contributors list cannot be empty" })
+    .refine((addresses) => addresses.every((val) => ethers.isAddress(val)), {
+      message: "All contributors must be valid Ethereum addresses",
+    }),
+  workStart: z
+    .number()
+    .int({ message: "workStart must be an integer" })
+    .positive({ message: "workStart must be positive" }),
+  workEnd: z
+    .number()
+    .int({ message: "workEnd must be an integer" })
+    .positive({ message: "workEnd must be positive" }),
+});
